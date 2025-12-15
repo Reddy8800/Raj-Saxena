ede0a05c3b1aec25356d030b34d331c463d180c5
codearvind07 <arvindfb07@gmail.com>
2025-12-15 11:02:26 +0530

    Added full project

---
+++ script.js
@@ -0,0 +1,14 @@
+// ---- UPDATE THESE VALUES ---- //
+
+document.getElementById("channelLogo").src = "logo.jpg";  
+document.getElementById("channelName").innerText = "RAJ SAXENA™ (ESTD 2016)"; 
+document.getElementById("subscriberCount").innerText = "86 813+";
+
+// Want auto-fetch subscribers?  
+// Tell me your channel username: @yourchannelname
+
+
+// ---- AUTO REDIRECT AFTER 5 SECONDS ---- //
+setTimeout(function () {
+    window.location.href = "https://t.me/+IvWQbocOydIzMjU1"; 
+}, 20000);
